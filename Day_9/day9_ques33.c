161e6c8a5927073d2564c191ecaa1f218f9a2177
Noorpreet <noorpreet.turan2007@gmail.com>
2026-06-10 05:38:52 +0530

    Day_9 uploaded

---
+++ Day_9/day9_ques33.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n , i , j ;
+
+    printf("input the number of rows : ");
+    scanf("%d",&n);
+
+    for( i=1 ; i<=n ; i++)
+    {
+        for( j=n ; j>=i ; j--)
+        {
+            printf(" * ");
+        }
+
+        printf("\n");
+    }
+    
+    return 0;
+}

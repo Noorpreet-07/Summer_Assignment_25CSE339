dc5aa84a8292a6283174de16dd7569f6bfeb299f
Noorpreet <noorpreet.turan2007@gmail.com>
2026-06-17 20:15:44 +0530

    Day_17 uploaded

---
+++ Day_17/day17_ques68.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n1, n2, i, j, k = 0;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+
+    int arr1[n1];
+
+    printf("Enter elements of first array:\n");
+    for(i = 0; i < n1; i++) 
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+
+    int arr2[n2];
+
+    printf("Enter elements of second array:\n");
+    for(i = 0; i < n2; i++) 
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    int common[n1]; 
+
+    printf("Common elements are:\n");
+
+    for(i = 0; i < n1; i++) 
+    {
+        for(j = 0; j < n2; j++) 
+        {
+            if(arr1[i] == arr2[j]) 
+            {
+                int found = 0;
+
+                for(int x = 0; x < k; x++) 
+                {
+                    if(common[x] == arr1[i]) 
+                    {
+                        found = 1;
+                        break;
+                    }
+                }
+
+                if(!found) 
+                {
+                    common[k++] = arr1[i];
+                    printf("%d ", arr1[i]);
+                }
+            }
+        }
+    }
+
+    return 0;
+}

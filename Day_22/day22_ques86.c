eaf0aee075ff66038026bf76a8cccc28128c2325
Noorpreet <noorpreet.turan2007@gmail.com>
2026-06-22 18:32:50 +0530

    Day_22 uploaded

---
+++ Day_22/day22_ques86.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[200];
+    int i, count = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+
+    for(i = 0; str[i] != '\0'; i++) 
+    {
+        
+        if((str[i] == ' ' || str[i] == '\n') && str[i+1] != ' ' && str[i+1] != '\0') 
+        {
+            count++;
+        }
+    }
+
+    if(str[0] != '\0' && str[0] != '\n') 
+    {
+        count++;
+    }
+
+    printf("Number of words = %d\n", count);
+
+    return 0;
+}
